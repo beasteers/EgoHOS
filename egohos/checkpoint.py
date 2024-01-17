e44e184f246008b59ce4ddfe4201624031e9eb7a
Bea Steers <bea.steers@gmail.com>
2024-01-17 13:09:00 -0500

    change weight urls to github release

--- egohos/checkpoint.py
+++ egohos/checkpoint.py
@@ -3,22 +3,31 @@ import os
 ROOT_DIR = os.path.dirname(os.path.dirname(__file__))
 MODEL_DIR = os.getenv("MODEL_DIR") or os.path.join(ROOT_DIR, 'weights')
 
-BASE_URL = 'https://docs.google.com/uc?export=download&confirm=t&id={}'
-FILE_ID = '1m7C_rWymUi045CpWkaXza8zWJ-Emys1X' #'1LNMQ6TGf1QaCjMgTExPzl7lFFs-yZyqX'
+BASE_URL = 'https://github.com/beasteers/EgoHOS/releases/download/v1/{}'
+FILES = ['seg_twohands_ccda', 'twohands_to_cb_ccda', 'twohands_cb_to_obj1_ccda', 'twohands_cb_to_obj2_ccda']
 
 def ensure_checkpoint(path=None):
     path = path or os.path.join(MODEL_DIR, 'egohos')
-    #print(path, os.path.isdir(path))
-    if not os.path.isdir(path):
-        os.makedirs(os.path.dirname(path), exist_ok=True)
+    os.makedirs(os.path.dirname(path), exist_ok=True)
 
-        print("No checkpoint found. Downloading...")
-        def show_progress(i, size, total):
-            print(f'downloading checkpoint to {path}: {i * size / total:.2%}', end="\r")
-        
-        import urllib.request
-        urllib.request.urlretrieve(BASE_URL.format(FILE_ID), 'work_dirs.zip', show_progress)
-        import zipfile
-        with zipfile.ZipFile('work_dirs.zip', 'r') as zf:
-            zf.extractall(path)
+    for name in FILES:
+        pathi = os.path.join(path, name)
+        if not os.path.isdir(pathi):
+            print("No checkpoint found. Downloading...")
+            def show_progress(i, size, total):
+                print(f'downloading checkpoint to {path}: {i * size / total:.2%}', end="\r")
+            
+            import urllib.request, zipfile
+            out_path = f'{name}.zip'
+            url = BASE_URL.format(f'{name}.zip')
+            urllib.request.urlretrieve(url, out_path, show_progress)
+            try:
+                with zipfile.ZipFile(out_path, 'r') as zf:
+                    zf.extractall(pathi)
+            except zipfile.BadZipFile:
+                print(url)
+                print(open(out_path).read(200))
+                raise
+            finally:
+                os.remove(out_path)
     return path
